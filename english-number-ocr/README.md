d2e25dfdb8dbbad7f78b9ed6dd56eb00db76aa2b
Vishal Talwar <rawlatv@gmail.com>
2016-03-08 18:38:31 -0800

    Updating data generation script with some args

---
+++ english-number-ocr/README.md
@@ -0,0 +1,11 @@
+```
+$ python generate_data.py data/fontdata_small.mat --num_fonts 200 --pixels 20
+
+Generating font-based training data...
+  [####################################]  100%
+Successfully created training data!
+
+$ python visualize_data.py data/fontdata_small.mat
+```
+
+![Sample training data](images/font-data-viz.jpg)
--- english-number-ocr/generate_data.py
+++ english-number-ocr/generate_data.py
@@ -4,21 +4,25 @@ import itertools
 
 import click
 import numpy as np
+import random
 import scipy.io as sio
 
 from en_utils import get_fonts, load_font, glyph_to_image, image_to_numpy
 
 
 @click.command()
+@click.option('--num_fonts', default=0, help='The number of fonts to generate data for')
+@click.option('--pixels', default=20, help='Both dimensions of the generated images in pixels')
 @click.argument('output')
-def generate_data(output):
-    fonts = get_fonts()[:10]
+def generate_data(output, num_fonts, pixels):
+    fonts = get_fonts()
+    if num_fonts:
+        fonts = random.sample(fonts, num_fonts)
     glyphs = '0123456789'
-    imscale = 20
 
     # Allocate space for training data.
     H = len(fonts) * len(glyphs)  # Number of training instances.
-    W = imscale * imscale         # Pixels in image.
+    W = pixels * pixels           # Pixels in image.
     X = np.zeros((H, W), dtype=np.float64)
     y = np.zeros((H, 1), dtype=np.uint8)
 
@@ -26,10 +30,10 @@ def generate_data(output):
     print("Generating font-based training data...")
     with click.progressbar(fonts) as pb:
         for i, font_path in enumerate(pb):
-            font = load_font(font_path, imscale - 4)
+            font = load_font(font_path, pixels - 4)
             for j, glyph in enumerate(glyphs):
                 try:
-                    im = glyph_to_image(glyph, font, imscale)
+                    im = glyph_to_image(glyph, font, pixels)
                     im_np = image_to_numpy(im)
                     idx = i * len(glyphs) + j
                     X[idx, :] = im_np
