639118177f2fa709d0b4600e3ce8da3ec8828071
Vishal Talwar <rawlatv@gmail.com>
2017-04-01 21:12:42 -0700

    Training working

--- pl-classifier/text_cnn.py
+++ pl-classifier/text_cnn.py
@@ -9,11 +9,15 @@ class TextCNN(object):
     Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
     """
     def __init__(self, sequence_length, num_classes, vocab_size,
-                 embedding_size, filter_sizes, num_filters):
+                 embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
         # Placeholders for input, output and dropout
         self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
-        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
+        self.input_y = tf.placeholder(tf.int32, [None, 1], name="input_y")
+        y_one_hot = tf.one_hot(self.input_y, num_classes)
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+
+        # Keeping track of l2 regularization loss (optional)
+        l2_loss = tf.constant(0.0)
 
         with tf.name_scope("embedding"):
             W = tf.Variable(
@@ -47,7 +51,7 @@ class TextCNN(object):
 
         # Combine all the pooled features
         num_filters_total = num_filters * len(filter_sizes)
-        self.h_pool = tf.concat(3, pooled_outputs)
+        self.h_pool = tf.concat(pooled_outputs, 3)
         self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
 
         # Add dropout
@@ -64,15 +68,16 @@ class TextCNN(object):
             l2_loss += tf.nn.l2_loss(W)
             l2_loss += tf.nn.l2_loss(b)
             self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
-            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+            self.predictions = tf.cast(tf.argmax(self.scores, 1, name="predictions"), tf.int32)
 
-        # CalculateMean cross-entropy loss
+        # Calculate  cross-entropy loss
         with tf.name_scope("loss"):
-            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            losses = tf.nn.softmax_cross_entropy_with_logits(
+                    logits=self.scores, labels=y_one_hot)
             self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
 
         # Accuracy
         with tf.name_scope("accuracy"):
-            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            correct_predictions = tf.equal(self.predictions, self.input_y)
             self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
 
--- pl-classifier/train.py
+++ pl-classifier/train.py
@@ -1,3 +1,8 @@
+###
+# Based on https://github.com/dennybritz/cnn-text-classification-tf/blob/master/train.py
+# Modified to use TF batching.
+###
+
 import tensorflow as tf
 import numpy as np
 import os
@@ -17,9 +22,9 @@ from text_cnn import TextCNN
 
 # Data loading params
 tf.flags.DEFINE_float('train_val_split', .1,
-		      'Percentage of the training data to use for validation')
+              'Percentage of the training data to use for validation')
 tf.flags.DEFINE_string('data_directory', './data',
-		       'Data source organized as one directory of files per label.')
+               'Data source organized as one directory of files per label.')
 tf.flags.DEFINE_integer('min_word_frequency', 25, 'Minimum number of occurrences needed to include a word from the data (default: 25)')
 
 # Model Hyperparameters
@@ -31,8 +36,8 @@ tf.flags.DEFINE_float('l2_reg_lambda', 0.0, 'L2 regularization lambda (default: 
 
 # Training parameters
 tf.flags.DEFINE_integer('batch_size', 64, 'Batch Size (default: 64)')
-tf.flags.DEFINE_integer('num_epochs', 200, 'Number of training epochs (default: 200)')
-tf.flags.DEFINE_integer('evaluate_every', 100, 'Evaluate model on dev set after this many steps (default: 100)')
+tf.flags.DEFINE_integer('num_epochs', 3, 'Number of training epochs (default: 3)')
+tf.flags.DEFINE_integer('evaluate_every', 100, 'Evaluate model on val set after this many steps (default: 100)')
 tf.flags.DEFINE_integer('checkpoint_every', 100, 'Save model after this many steps (default: 100)')
 tf.flags.DEFINE_integer('num_checkpoints', 5, 'Number of checkpoints to store (default: 5)')
 # Misc Parameters
@@ -41,10 +46,10 @@ tf.flags.DEFINE_boolean('log_device_placement', False, 'Log placement of ops on 
 
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
-print('\nParameters:')
+print('Parameters:')
 for attr, value in sorted(FLAGS.__flags.items()):
     print('{}={}'.format(attr.upper(), value))
-print('')
+print()
 
 
 # Data Preparation
@@ -52,8 +57,9 @@ print('')
 
 # Load data.
 print('Loading data:')
-text_data = data.load_data(FLAGS.data_directory, per_label_file_limit=10)
+text_data = data.load_data(FLAGS.data_directory, per_label_file_limit=100)
 x_text, y, labels = data.label_data(text_data)
+print('Num labels: {}'.format(len(labels)))
 print()
 
 # Build vocabulary.
@@ -68,24 +74,150 @@ vocab_processor = learn.preprocessing.VocabularyProcessor(
         tokenizer_fn=passthrough_tokenizer,
         min_frequency=FLAGS.min_word_frequency)
 x = np.array(list(vocab_processor.fit_transform(x_text)))
-y = np.array(y)
+y = np.array(y).reshape((len(y), 1))
 print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
 
-#x_train, y_train, x_test, y_test = data.split_data(FLAGS.train_val_split, shuffle=True)
 # Shuffle data.
 shuffle_indices = np.random.permutation(np.arange(len(y)))
 x_shuffled = x[shuffle_indices]
 y_shuffled = y[shuffle_indices]
 
 # Split train/test set.
-dev_sample_index = -int(FLAGS.train_val_split * len(y))
-x_train, x_val = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
-y_train, y_val = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
+val_sample_index = -int(FLAGS.train_val_split * len(y))
+x_train, x_val = x_shuffled[:val_sample_index], x_shuffled[val_sample_index:]
+y_train, y_val = y_shuffled[:val_sample_index], y_shuffled[val_sample_index:]
 print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_val)))
 
+with tf.Graph().as_default():
+    session_conf = tf.ConfigProto(
+        allow_soft_placement=FLAGS.allow_soft_placement,
+        log_device_placement=FLAGS.log_device_placement)
+    sess = tf.Session(config=session_conf)
+    with sess.as_default():
+        cnn = TextCNN(
+                sequence_length=x_train.shape[1],
+                num_classes=len(labels),
+                vocab_size=len(vocab_processor.vocabulary_),
+                embedding_size=FLAGS.embedding_dim,
+                filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
+                num_filters=FLAGS.num_filters,
+                l2_reg_lambda=FLAGS.l2_reg_lambda)
 
-# Send split% to training, (100-split)% to test.
-#split = int(train_test_split * len(lines))
-#train_data[lang].extend(lines[:split])
-#test_data[lang].extend(lines[split:])
+        # Define training procedure
+        global_step = tf.Variable(0, name="global_step", trainable=False)
+        optimizer = tf.train.AdamOptimizer(1e-3)
+        grads_and_vars = optimizer.compute_gradients(cnn.loss)
+        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+
+        # Keep track of gradient values and sparsity (optional)
+        grad_summaries = []
+        for g, v in grads_and_vars:
+            if g is not None:
+                grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(v.name), g)
+                sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
+                grad_summaries.append(grad_hist_summary)
+                grad_summaries.append(sparsity_summary)
+        grad_summaries_merged = tf.summary.merge(grad_summaries)
+
+        # Output directory for models and summaries
+        timestamp = str(int(time.time()))
+        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+        print("Writing to {}\n".format(out_dir))
+
+        # Summaries for loss and accuracy
+        loss_summary = tf.summary.scalar("loss", cnn.loss)
+        acc_summary = tf.summary.scalar("accuracy", cnn.accuracy)
+
+        # Train Summaries
+        train_summary_op = tf.summary.merge([loss_summary, acc_summary, grad_summaries_merged])
+        train_summary_dir = os.path.join(out_dir, "summaries", "train")
+        train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+
+        # Val summaries
+        val_summary_op = tf.summary.merge([loss_summary, acc_summary])
+        val_summary_dir = os.path.join(out_dir, "summaries", "val")
+        val_summary_writer = tf.summary.FileWriter(val_summary_dir, sess.graph)
+
+        # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
+        checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+        checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=FLAGS.num_checkpoints)
+
+        # Write vocabulary
+        vocab_processor.save(os.path.join(out_dir, "vocab"))
+
+        def train_step(x_batch, y_batch):
+            """
+            A single training step
+            """
+            feed_dict = {
+              cnn.input_x: x_batch,
+              cnn.input_y: y_batch,
+              cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
+            }
+            _, step, summaries, loss, accuracy = sess.run(
+                [train_op, global_step, train_summary_op, cnn.loss, cnn.accuracy],
+                feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            train_summary_writer.add_summary(summaries, step)
+
+        def val_step(x_batch, y_batch, writer=None):
+            """
+            Evaluates model on a val set
+            """
+            feed_dict = {
+              cnn.input_x: x_batch,
+              cnn.input_y: y_batch,
+              cnn.dropout_keep_prob: 1.0
+            }
+            step, summaries, loss, accuracy = sess.run(
+                [global_step, val_summary_op, cnn.loss, cnn.accuracy],
+                feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            if writer:
+                writer.add_summary(summaries, step)
+
+        # Generate batches
+        x_train_single, y_train_single = tf.train.slice_input_producer(
+                [x_train, y_train],
+                num_epochs=FLAGS.num_epochs,
+                shuffle=False)
+        x_train_batches, y_train_batches = tf.train.shuffle_batch(
+                [x_train_single, y_train_single],
+                batch_size=FLAGS.batch_size,
+                capacity=2000,
+                min_after_dequeue=1000)
+
+        # Initialize all variables
+        sess.run(tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()))
+
+        # Training loop.
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        try:
+            while not coord.should_stop():
+                x_train_batch, y_train_batch = sess.run([x_train_batches,
+                                                         y_train_batches])
+                train_step(x_train_batch, y_train_batch)
+                current_step = tf.train.global_step(sess, global_step)
+                if current_step % FLAGS.evaluate_every == 0:
+                    print('\nEvaluation:')
+                    val_step(x_val, y_val, writer=val_summary_writer)
+                    print()
+                if current_step % FLAGS.checkpoint_every == 0:
+                    path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+                    print('Saved model checkpoint to {}\n'.format(path))
+        except tf.errors.OutOfRangeError:
+            print('Done training for %d epochs.' % (FLAGS.num_epochs,))
+        finally:
+            # When done, ask the threads to stop.
+            coord.request_stop()
+
+        # Wait for threads to finish.
+        coord.join(threads)
+        sess.close()
 
